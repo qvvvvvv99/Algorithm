4671d3836a428e2206321ada0b458560df7bf5b5
qvvvvvv99 <qvvvvvv99@naver.com>
2022-02-13 15:40:38 +0900

    Bactracking - N과 M 문제 정복

---
+++ src/FastCampus/Backtracking/NM_4.java
@@ -0,0 +1,51 @@
+package FastCampus.Backtracking;
+
+// BJ 15652 N°ú M (4)
+
+import java.io.*;
+import java.util.*;
+
+public class NM_4 {
+	static int N;
+	static int M;
+	static int[] ans;
+	static StringBuffer sb = new StringBuffer();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		StringTokenizer st = new StringTokenizer(input);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		ans = new int[M];
+
+		dfs(0);
+
+		System.out.println(sb);
+	}
+
+	public static void dfs(int idx) {
+		if (idx >= M) {
+			for (int i = 0; i < M; i++)
+				sb.append(ans[i] + " ");
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			ans[idx] = i;
+			if (check(idx))
+				dfs(idx + 1);
+		}
+	}
+
+	public static boolean check(int idx) {
+		if (idx > 0) {
+			if (ans[idx] < ans[idx - 1])
+				return false;
+		}
+		return true;
+	}
+
+}
---
+++ src/FastCampus/Backtracking/NM_5.java
@@ -0,0 +1,59 @@
+package FastCampus.Backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class NM_5 {
+	static int N;
+	static int M;
+	static int[] ans;
+	static int[] list;
+	static StringBuffer sb = new StringBuffer();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		StringTokenizer st = new StringTokenizer(input);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		list = new int[N];
+		ans = new int[M];
+		
+		input = br.readLine();
+		st = new StringTokenizer(input);
+		for(int i = 0; i < N; i++)
+			list[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(list);
+
+		dfs(0);
+
+		System.out.println(sb);
+	}
+
+	public static void dfs(int idx) {
+		if (idx >= M) {
+			for (int i = 0; i < M; i++)
+				sb.append(ans[i] + " ");
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			ans[idx] = list[i];
+			if (check(idx))
+				dfs(idx + 1);
+		}
+	}
+
+	public static boolean check(int idx) {
+		for(int i = 0; i < idx; i++) {
+			if(ans[i] == ans[idx])
+				return false;
+		}
+		return true;
+	}
+
+}
