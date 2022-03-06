48c68146bad8721d2910e97788040903ba7ec00b
qvvvvvv99 <qvvvvvv99@naver.com>
2022-03-06 17:42:39 +0900

    수정

--- src/BJAlgo/Level3/BJ6064.java
+++ src/BJAlgo/Level3/BJ6064.java
@@ -7,40 +7,28 @@ public class BJ6064 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int tc = Integer.parseInt(br.readLine());
-		StringBuffer sb = new StringBuffer();
-		
-		for(int i = 0; i < tc; i++) {
-			String line = br.readLine();
-			StringTokenizer st = new StringTokenizer(line);
-			int M = Integer.parseInt(st.nextToken());
-			int N = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			
-			int minus = M - N;
-			int temp = x;
-			int cnt = x;
-			int lcm = M*N/gcd(M,N);
-			while(cnt < lcm) {
-				temp += minus;
-				if(temp < 0)
-					temp+=N;
-				if(temp > N)
-					temp-=N;
-				cnt+=M;
-				if(temp == y) 
-					break;
-			}
-			if (cnt > lcm)
-				cnt = -1;
-			sb.append(cnt+"\n");
-		}
-		br.close();
-		System.out.print(sb);
-	}
-	static int gcd(int n1, int n2) {
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            int M = sc.nextInt(), N = sc.nextInt(), x = sc.nextInt(), y = sc.nextInt();
+
+            int lcm = M * N / gcd(M, N);
+            int n = 0;
+            int ans = -1;
+            while (n * M < lcm) {
+                if ((n * M + x - y) % N == 0) {
+                    ans = n * M + x;
+                    break;
+                }
+                n++;
+            }
+
+            System.out.println(ans);
+        }
+    }
+
+    static int gcd(int n1, int n2) {
         if (n2 == 0)
             return n1;
         return gcd(n2, n1 % n2);
