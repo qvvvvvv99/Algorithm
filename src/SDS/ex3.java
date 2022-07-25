12385ef1eb928807f2cb1c7f9a771a6d7da2269c
KimJH <qvvvvvv99@naver.com>
2022-07-25 15:59:37 +0900

    sds 알고특강 6일차

--- src/SDS/ex3.java
+++ src/SDS/ex3.java
@@ -25,9 +25,6 @@ public class ex3 {
 			for(int i = 1; i < 17; i++) {
 				int res = 0;
 				int num = 0;
-				boolean[] check = new boolean[8];
-				boolean[] visited = new boolean[17];
-				bfs(cost, i, check, visited, res, num);
 			}
 			int idx = t+1;
 			sb.append("#"+ idx + " " + result +"\n");
@@ -36,130 +33,8 @@ public class ex3 {
 
 	}
 	
-	public static void bfs(int[] cost, int root, boolean[] check, boolean[] visited, int res, int num) {
-		res += cost[root];
-		visit(root, check);
-		visited[root] = true;
+	public static void bfs(int res, int num) {
 		num += 1;
-		
-		int cnt;
-		if(num >= N) {
-			for(cnt = 0; cnt < 8; cnt++) {
-				if (check[cnt] != true)
-					break;
-			}
-			if(cnt == 8) {
-				if(result > res)
-					result = res;
-				return;
-			}
-		}
-		
-		for(int i = 1; i < 17; i++) {
-			if(visited[i] == false)
-				bfs(cost, i, check, visited, res, num);
-		}
-	}
-	
-	public static void visit(int root, boolean[] check) {
-		switch(root) {
-		case 1:
-			check[0] = true;
-			check[2] = true;
-			check[4] = true;
-			check[6] = true;
-			break;
-		case 2:
-			check[0] = true;
-			check[2] = true;
-			check[4] = true;
-			check[7] = true;
-			break;
-		case 3:
-			check[0] = true;
-			check[2] = true;
-			check[5] = true;
-			check[6] = true;
-			break;
-		case 4:
-			check[0] = true;
-			check[2] = true;
-			check[5] = true;
-			check[7] = true;
-			break;
-		case 5:
-			check[0] = true;
-			check[3] = true;
-			check[4] = true;
-			check[6] = true;
-			break;
-		case 6:
-			check[0] = true;
-			check[3] = true;
-			check[4] = true;
-			check[7] = true;
-			break;
-		case 7:
-			check[0] = true;
-			check[3] = true;
-			check[5] = true;
-			check[6] = true;
-			break;
-		case 8:
-			check[0] = true;
-			check[3] = true;
-			check[5] = true;
-			check[7] = true;
-			break;
-		case 9:
-			check[1] = true;
-			check[2] = true;
-			check[4] = true;
-			check[6] = true;
-			break;
-		case 10:
-			check[1] = true;
-			check[2] = true;
-			check[4] = true;
-			check[7] = true;
-			break;
-		case 11:
-			check[1] = true;
-			check[2] = true;
-			check[5] = true;
-			check[6] = true;
-			break;
-		case 12:
-			check[1] = true;
-			check[2] = true;
-			check[5] = true;
-			check[7] = true;
-			break;
-		case 13:
-			check[1] = true;
-			check[3] = true;
-			check[4] = true;
-			check[6] = true;
-			break;
-		case 14:
-			check[1] = true;
-			check[3] = true;
-			check[4] = true;
-			check[7] = true;
-			break;
-		case 15:
-			check[1] = true;
-			check[3] = true;
-			check[5] = true;
-			check[6] = true;
-			break;
-		case 16:
-			check[1] = true;
-			check[3] = true;
-			check[5] = true;
-			check[7] = true;
-			break;
-		}
 		
 	}
 }
