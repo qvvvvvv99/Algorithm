7e9302ec9d1cc2b6cbcf2763b2fdea7d145cbe69
KimJH <qvvvvvv99@naver.com>
2022-09-07 14:57:51 +0900

    백준 문자열

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,36 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int cnt = N;
+		boolean[] flag = new boolean[26];
 
-		String str = sc.nextLine();
-		int cnt = 0;
-		for (int i = 0; i < str.length(); i++) {
-			cnt++;
-			if (str.charAt(i) == 'c') {
-				if (i + 1 < str.length()) {
-					if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
-						i++;
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			Arrays.fill(flag, false);
+			flag[str.charAt(0) - 'a'] = true;
+			for (int j = 1; j < str.length(); j++) {
+				if(str.charAt(j) == str.charAt(j-1)) 
+					continue;
+				if(flag[str.charAt(j)-'a'] == true) {
+					cnt--;
+					break;
 				}
-			} else if (str.charAt(i) == 'l' || str.charAt(i) == 'n') {
-				if (i + 1 < str.length()) {
-					if (str.charAt(i + 1) == 'j')
-						i++;
-				}
-			} else if (str.charAt(i) == 's' || str.charAt(i) == 'z') {
-				if (i + 1 < str.length()) {
-					if (str.charAt(i + 1) == '=')
-						i++;
-				}
-			} else if (str.charAt(i) == 'd') {
-				if (i + 1 < str.length()) {
-					if (str.charAt(i + 1) == 'z') {
-						if (i + 2 < str.length()) {
-							if (str.charAt(i + 2) == '=')
-								i += 2;
-						}
-					} else if (str.charAt(i + 1) == '-')
-						i++;
-				}
+				flag[str.charAt(j) - 'a'] = true;
 			}
 		}
 
