ed0221c3dc913d70da0ced04c44badb421ad9fcf
KimJH <qvvvvvv99@naver.com>
2022-09-07 14:44:28 +0900

    백준 문자열

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
-	static int[] dial = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		String str = sc.nextLine();
-		int sum = 0;
-		for(int i = 0; i < str.length(); i++) {
-			sum += dial[str.charAt(i) - 'A'];
+		int cnt = 0;
+		for (int i = 0; i < str.length(); i++) {
+			cnt++;
+			if (str.charAt(i) == 'c') {
+				if (i + 1 < str.length()) {
+					if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
+						i++;
+				}
+			} else if (str.charAt(i) == 'l' || str.charAt(i) == 'n') {
+				if (i + 1 < str.length()) {
+					if (str.charAt(i + 1) == 'j')
+						i++;
+				}
+			} else if (str.charAt(i) == 's' || str.charAt(i) == 'z') {
+				if (i + 1 < str.length()) {
+					if (str.charAt(i + 1) == '=')
+						i++;
+				}
+			} else if (str.charAt(i) == 'd') {
+				if (i + 1 < str.length()) {
+					if (str.charAt(i + 1) == 'z') {
+						if (i + 2 < str.length()) {
+							if (str.charAt(i + 2) == '=')
+								i += 2;
+						}
+					} else if (str.charAt(i + 1) == '-')
+						i++;
+				}
+			}
 		}
-		
-		System.out.println(sum);
+
+		System.out.println(cnt);
 	}
 
 }
