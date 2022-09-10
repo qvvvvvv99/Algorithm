c6767819d0638ca46d33e37712ec0a75f4cd4103
KimJH <qvvvvvv99@naver.com>
2022-09-10 23:42:40 +0900

    백준 수학

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,26 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int cnt = N;
-		boolean[] flag = new boolean[26];
-
-		for (int i = 0; i < N; i++) {
-			String str = sc.next();
-			Arrays.fill(flag, false);
-			flag[str.charAt(0) - 'a'] = true;
-			for (int j = 1; j < str.length(); j++) {
-				if(str.charAt(j) == str.charAt(j-1)) 
-					continue;
-				if(flag[str.charAt(j)-'a'] == true) {
-					cnt--;
-					break;
-				}
-				flag[str.charAt(j) - 'a'] = true;
-			}
-		}
-
-		System.out.println(cnt);
+		int A = sc.nextInt();	// 고정
+		int B = sc.nextInt();	// 가변
+		int C = sc.nextInt();	// 판매가격
+		int res;
+		
+		// A + n * B < C * n -> n의 최소
+		if(B >= C)
+			res = -1;
+		else 
+			res = A / (C - B) + 1;
+		
+		System.out.println(res);
 	}
 
 }
