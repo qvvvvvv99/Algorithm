e7158baa0dfe5277f1e18451c7d6340e5ff12b2e
KimJH <qvvvvvv99@naver.com>
2022-09-11 15:18:04 +0900

    알고리즘 스터디 8주차 문제풀이

--- src/AlgoStudy/Week8/BJ19584_v2.java
+++ src/AlgoStudy/Week8/BJ19584_v2.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class BJ19584 {
+public class BJ19584_v2 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -19,71 +18,47 @@ public class BJ19584 {
 		int N = Integer.parseInt(st.nextToken()); // 좌표 개수
 		int M = Integer.parseInt(st.nextToken()); // 도로 수
 		int[] y = new int[N + 1];
+		HashSet<Integer> hs = new HashSet<>();
 
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			st.nextToken();
 			y[i] = Integer.parseInt(st.nextToken());
+			hs.add(y[i]);
 		}
 
-		LinkedList<RouteInfo> route = new LinkedList<>();
-		int up, under, price;
+		ArrayList<Integer> list = new ArrayList<>(hs);
+		Collections.sort(list);
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for(int i = 0; i < list.size(); i++)
+			hm.put(list.get(i), i+1);
+		
+		long[] pri = new long[list.size()+2];
+		int up, under;
+		long price;
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			up = y[Integer.parseInt(st.nextToken())];
 			under = y[Integer.parseInt(st.nextToken())];
+			up = y[Integer.parseInt(st.nextToken())];
 			price = Integer.parseInt(st.nextToken());
 			if (up < under) {
 				int temp = up;
 				up = under;
 				under = temp;
 			}
-			route.add(new RouteInfo(up, under, price));
+			under = hm.get(under);
+			up = hm.get(up);
+			pri[under] += price;
+			pri[up+1] -= price;
 		}
-
-		Collections.sort(route);
-
-		int res = 0, max = 0;
-		int top = 0, bottom = 0;
-		int num;
-		while (true) {
-			num = route.get(top).up;
-			while (bottom < M && num == route.get(bottom).up) {
-				res += route.get(bottom).price;
-				bottom++;
-			}
-			max = Math.max(res, max);
-			if(bottom == M)
-				break;
-			num = route.get(top).under;
-			while (top < M && num == route.get(top).under) {
-				res -= route.get(top).price;
-				top++;
-			}
+		
+		long max = pri[1];
+		for(int i = 2; i < pri.length-1; i++) {
+			pri[i] += pri[i-1];
+			max = Math.max(max, pri[i]);
 		}
-
+		
 		System.out.println(max);
-	}
-
-}
-
-class RouteInfo implements Comparable<RouteInfo> {
-	int up;
-	int under;
-	int price;
-
-	RouteInfo(int up, int under, int price) {
-		this.up = up;
-		this.under = under;
-		this.price = price;
-	}
-
-	@Override
-	public int compareTo(RouteInfo r) {
-		// TODO Auto-generated method stub
-		if (this.up == r.up)
-			return r.under - this.under;
-		return r.up - this.up;
 	}
 
 }
