4cfab9c5a3bfc1e706e45095f0e5c18d589d830a
KimJH <qvvvvvv99@naver.com>
2022-09-14 23:51:51 +0900

    백준 수학

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -5,18 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();	// 고정
-		int B = sc.nextInt();	// 가변
-		int C = sc.nextInt();	// 판매가격
-		int res;
+		BigInteger num1 = new BigInteger(sc.next());
+		BigInteger num2 = new BigInteger(sc.next());
 		
-		// A + n * B < C * n -> n의 최소
-		if(B >= C)
-			res = -1;
-		else 
-			res = A / (C - B) + 1;
-		
-		System.out.println(res);
+		System.out.println(num1.add(num2));
 	}
 
 }
