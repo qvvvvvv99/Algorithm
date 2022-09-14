057b5bcd9c99aa6035d71f217fdee6ed504540ec
KimJH <qvvvvvv99@naver.com>
2022-09-15 00:08:53 +0900

    백준 기본 수학 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -6,10 +5,27 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		BigInteger num1 = new BigInteger(sc.next());
-		BigInteger num2 = new BigInteger(sc.next());
+		int X = sc.nextInt();
+		int n = 0, idx = 1;
+		int ja, mo;
 		
-		System.out.println(num1.add(num2));
+		while(true) {
+			if(n >= X)
+				break;
+			
+			n += idx;
+			idx++;
+		}
+		n -= (idx - 1);
+		if(idx % 2 == 1) {
+			mo = idx - (X - n);
+			ja = X - n;
+		}else {
+			mo = X - n;
+			ja = idx - (X - n);
+		}
+		
+		System.out.println(ja+"/"+mo);
 	}
 
 }
