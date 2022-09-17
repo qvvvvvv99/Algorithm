914d907675fdd234ed652131ef44ca07478aef82
KimJH <qvvvvvv99@naver.com>
2022-09-17 19:11:03 +0900

    백준 단계별로 풀어보기

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,16 +9,27 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int k= sc.nextInt();
-		int[] ary = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			ary[i] = sc.nextInt();
+
+		if(N > 1) {
+			ArrayList<Integer> list = new ArrayList<>();
+			while(true) {
+				int i;
+				for(i = 2; i <= (int)Math.sqrt(N); i++) {
+					if(N % i == 0) {
+						N /= i;
+						list.add(i);
+						break;
+					}
+				}
+				if(i > (int)Math.sqrt(N)) {
+					list.add(N);
+					break;
+				}
+			}
+			
+			for(int i = 0; i < list.size(); i++)
+				System.out.println(list.get(i));
 		}
-		
-		Arrays.sort(ary);
-		
-		System.out.println(ary[N-k]);
 	}
 
 }
