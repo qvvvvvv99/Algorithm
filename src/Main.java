5583265609abf1580acfce12cf2c0d60b463d980
KimJH <qvvvvvv99@naver.com>
2022-09-19 16:17:51 +0900

    백준 단계별로 문제풀기

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-	static int sum;
-	static int min;
-	static boolean flag;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int M = sc.nextInt();
-		int N = sc.nextInt();
-		
-		for(int i = N; i >= M; i--) {
-			if(i % 2 == 0 || i == 1)
-				continue;
-			if (isPrime(i)) {
-				sum += i;
-				min = i;
+		StringBuffer sb = new StringBuffer();
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int i;
+			for (i = N / 2; i >= 2; i--) {
+				if (isPrime(i) == true && isPrime(N-i) == true) {
+					break;
+				}
 			}
+			sb.append(i + " " + (N-i) + "\n");
 		}
-		
-		if(M <= 2) {
-			sum += 2;
-			min = 2;
-			flag = true;
-		}
-		if(N == 1)
-			flag = false;
-		
-		if(flag == false)
-			System.out.println(-1);
-		else
-			System.out.println(sum + "\n" + min);
+
+		System.out.println(sb);
 	}
 
 	public static boolean isPrime(int num) {
-		for(int i = 2; i <= (int)Math.sqrt(num); i++) {
-			if(num % i == 0)
+		for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+			if (num % i == 0)
 				return false;
 		}
-		flag = true;
 		return true;
 	}
 }
