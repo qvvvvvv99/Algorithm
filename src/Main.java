b047d8132e99514f8705030baf75891f0654c843
KimJH <qvvvvvv99@naver.com>
2022-09-20 18:24:19 +0900

    재귀

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
-		int T = sc.nextInt();
-		for (int tc = 0; tc < T; tc++) {
-			int N = sc.nextInt();
-			int i;
-			for (i = N / 2; i >= 2; i--) {
-				if (isPrime(i) == true && isPrime(N-i) == true) {
-					break;
-				}
-			}
-			sb.append(i + " " + (N-i) + "\n");
-		}
-
-		System.out.println(sb);
+		int N = sc.nextInt();
+		
+		System.out.println(factorial(N));
 	}
 
-	public static boolean isPrime(int num) {
-		for (int i = 2; i <= (int) Math.sqrt(num); i++) {
-			if (num % i == 0)
-				return false;
-		}
-		return true;
+	public static int factorial(int n) {
+		if(n <= 1)
+			return 1;
+		return n * factorial(n-1);
 	}
 }
+
+//
+//public static boolean isPrime(int num) {
+//	for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+//		if (num % i == 0)
+//			return false;
+//	}
+//	return true;
+//}
