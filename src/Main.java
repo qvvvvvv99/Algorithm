4842cd84891a6a4c8710daf17f7821887cbaf716
KimJH <qvvvvvv99@naver.com>
2022-10-01 19:12:55 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        int b = N % 10;
-        N /= 10;
-        if(b == 0){
-            b = N % 10;
-            b *= 10;
-            N /= 10;
-        }
-        int a = N;
-        
-        System.out.println(a+b);
-    }
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int t = 0; t < n; t++) {
+			String str = sc.next();
+
+			int sum1 = 0, sum2 = 0;
+
+			for (int i = 0; i < 3; i++)
+				sum1 += (str.charAt(i) - 'A') * Math.pow(26, 2 - i);
+
+			String[] s = str.split("-");
+			sum2 = Integer.parseInt(s[1]);
+
+			if(Math.abs(sum1-sum2) <= 100)
+				System.out.println("nice");
+			else
+				System.out.println("not nice");
+		}
+	}
 }
 
 //
