7230c9a2c70bef8f761d54c86b5cd9aea479436b
KimJH <qvvvvvv99@naver.com>
2022-10-04 20:57:48 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println("int a;");
-		System.out.println("int *ptr = &a;");
-
-		if(n >= 2) 
-			System.out.println("int **ptr2 = &ptr;");
+		int p = sc.nextInt();
 		
-		String pointer = "**";
-		for(int i = 3; i <= n; i++) {
-			pointer += "*";
-			System.out.println("int "+ pointer + "ptr"+i +" = &ptr"+(i-1)+";");
-		}
+		int min = p;
+		
+		if(n >= 5)
+			min = p - 500;
+		if(n >= 10)
+			min = Math.min(min, p * 9 / 10);
+		if(n >= 15)
+			min = Math.min(p - 2000, min);
+		if(n >= 20)
+			min = Math.min(min, p * 75 / 100);
+		if(min < 0)
+			min = 0;
+		
+		System.out.println(min);
 	}
 }
 
