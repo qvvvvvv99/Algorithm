a9b67818a55624fe5361b7ba1ad92a817f27f1c7
KimJH <qvvvvvv99@naver.com>
2022-10-08 01:13:48 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		 // 현재 날짜 구하기
-		 LocalDate now = LocalDate.now();         
-		 // 포맷 정의        
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		 // 포맷 적용       
-		 String formatedNow = now.format(formatter);         
-		 // 결과 출력       
-		 System.out.println(formatedNow);  
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println(sc.nextInt() + sc.nextInt());  
 	}
+	// https://www.acmicpc.net/problem/2558
 }
 
 //
