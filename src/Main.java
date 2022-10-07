9a43a4d8f3d58631f8ceba41439d98bd756f3ada
KimJH <qvvvvvv99@naver.com>
2022-10-07 09:47:11 +0900

    백준 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int p = sc.nextInt();
-		
-		int min = p;
-		
-		if(n >= 5)
-			min = p - 500;
-		if(n >= 10)
-			min = Math.min(min, p * 9 / 10);
-		if(n >= 15)
-			min = Math.min(p - 2000, min);
-		if(n >= 20)
-			min = Math.min(min, p * 75 / 100);
-		if(min < 0)
-			min = 0;
-		
-		System.out.println(min);
+		 // 현재 날짜 구하기
+		 LocalDate now = LocalDate.now();         
+		 // 포맷 정의        
+		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		 // 포맷 적용       
+		 String formatedNow = now.format(formatter);         
+		 // 결과 출력       
+		 System.out.println(formatedNow);  
 	}
 }
 
