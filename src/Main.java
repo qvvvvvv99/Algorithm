ef10c1b072edf69dd5a6606c8ba9b32d1f264e3c
KimJH <qvvvvvv99@naver.com>
2022-10-14 14:49:56 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-
-		for(int i = n; i > 0; i--) {
-			for(int j = i; j > 0; j--) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		int hour = sc.nextInt();
+		int min = sc.nextInt();
+		int sec = sc.nextInt();
+		int time = sc.nextInt();
+		
+		sec += time;
+		min += sec / 60;
+		sec %= 60;
+		hour += min / 60;
+		min %= 60;
+		hour %= 24;
+		
+		System.out.println(hour + " " + min + " " + sec);
 	}
 }
 
