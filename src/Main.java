d5183fb5ecc88dc2a4d9c137e8521dac54142db0
KimJH <qvvvvvv99@naver.com>
2022-10-16 21:03:26 +0900

    백준 문제풀이 - 수학 공식 적용

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		String answer;
-		String input;
-		for (int i = 0; i < n; i++) {
-			answer = "";
-			input = sc.next();
-			answer += input.charAt(0);
-			answer += input.charAt(input.length()-1);
+		StringBuffer sb = new StringBuffer();
+		int T = sc.nextInt();
+		for(int tc = 0; tc < T; tc++) {
+			long R = sc.nextLong();
+			long C = sc.nextLong();
+			long min = Math.min(R, C);
+//			long max = Math.max(R, C);
 			
-			System.out.println(answer);
+			long white = (2*min-1)*R*C - min*(min-1)*(R+C) + (min-1)*min*(2*min-1)/3;
+			long dark = 2*min*R*C - min*min*(R+C) + min*(min+1)*(2*min+1)/3 - min*(min+1);
+//			long white = (long)((min*(min+1)*(2*min+1))/3 - min*min + (min*min)*(max-min));
+//			long dark = (long)((min*(min+1)*(2*min+1))/3 - min*min + (min*min)*(max-min)-min);
+			
+			sb.append(white + " " + dark+"\n");
 		}
+		
+		System.out.print(sb);
 	}
 }
 
