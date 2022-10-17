c9e4b71f469cbb84bf7bcf4fec1b4df81a48dee5
KimJH <qvvvvvv99@naver.com>
2022-10-17 19:48:34 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		StringBuffer sb = new StringBuffer();
-		int T = sc.nextInt();
-		for(int tc = 0; tc < T; tc++) {
-			long R = sc.nextLong();
-			long C = sc.nextLong();
-			long min = Math.min(R, C);
-//			long max = Math.max(R, C);
-			
-			long white = (2*min-1)*R*C - min*(min-1)*(R+C) + (min-1)*min*(2*min-1)/3;
-			long dark = 2*min*R*C - min*min*(R+C) + min*(min+1)*(2*min+1)/3 - min*(min+1);
-//			long white = (long)((min*(min+1)*(2*min+1))/3 - min*min + (min*min)*(max-min));
-//			long dark = (long)((min*(min+1)*(2*min+1))/3 - min*min + (min*min)*(max-min)-min);
-			
-			sb.append(white + " " + dark+"\n");
-		}
+		int ax = sc.nextInt();
+		int ay = sc.nextInt();
+		int az = sc.nextInt();
+		int cx = sc.nextInt();
+		int cy = sc.nextInt();
+		int cz = sc.nextInt();
+		int bx, by, bz;
+		bx = cx - az;
+		by = cy / ay;
+		bz = cz - ax;
 		
-		System.out.print(sb);
+		System.out.println(bx + " " + by + " " + bz);
 	}
 }
 
