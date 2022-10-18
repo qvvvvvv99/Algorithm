a9f6755d36368f4a481ef59abd2fc11b76bafa3a
KimJH <qvvvvvv99@naver.com>
2022-10-18 22:12:03 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int ax = sc.nextInt();
-		int ay = sc.nextInt();
-		int az = sc.nextInt();
-		int cx = sc.nextInt();
-		int cy = sc.nextInt();
-		int cz = sc.nextInt();
-		int bx, by, bz;
-		bx = cx - az;
-		by = cy / ay;
-		bz = cz - ax;
-		
-		System.out.println(bx + " " + by + " " + bz);
-	}
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        StringBuffer sb = new StringBuffer();
+        
+        for(int tc = 0; tc < T; tc++){
+            int n = sc.nextInt();
+            for(int i = 0; i < n; i++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int plus = a + b;
+                int multi = a * b;
+                sb.append(plus + " " + multi+"\n");
+            }
+        }
+        
+        System.out.print(sb);
+    }
 }
-
 //
 //public static boolean isPrime(int num) {
 //	for (int i = 2; i <= (int) Math.sqrt(num); i++) {
