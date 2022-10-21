3061ad0897a3e5c9accb49c2291e8b8bf41ce14f
KimJH <qvvvvvv99@naver.com>
2022-10-21 23:54:28 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        StringBuffer sb = new StringBuffer();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        int b = sc.nextInt();
         
-        for(int tc = 0; tc < T; tc++){
-            int n = sc.nextInt();
-            for(int i = 0; i < n; i++){
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                int plus = a + b;
-                int multi = a * b;
-                sb.append(plus + " " + multi+"\n");
-            }
-        }
+        a /= 2;
         
-        System.out.print(sb);
+        System.out.println(Math.min(a,b));
     }
 }
-//
+
 //public static boolean isPrime(int num) {
 //	for (int i = 2; i <= (int) Math.sqrt(num); i++) {
 //		if (num % i == 0)
