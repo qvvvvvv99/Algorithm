6d29f908695e7cad01273ba036b80c3f929e03b7
KimJH <qvvvvvv99@naver.com>
2022-11-05 14:01:46 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String c = sc.next();
+		//Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] res = new int[N][M];
+        StringBuffer sb = new StringBuffer();
+        
+        for(int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j = 0; j < M; j++) {
+        		res[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        for(int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j = 0; j < M; j++) {
+        		res[i][j] += Integer.parseInt(st.nextToken());
+        		sb.append(res[i][j] + " ");
+        	}
+        	sb.append("\n");
+        }
+        
 		
-		if(c.equals("N") || c.equals("n"))
-			System.out.println("Naver D2");
-		else
-			System.out.println("Naver Whale");
+		System.out.println(sb);
     }
 }
 
