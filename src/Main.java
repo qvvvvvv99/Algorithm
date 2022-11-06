a1ad75d296659f671cf443e48e0d49d2b616f2a2
KimJH <qvvvvvv99@naver.com>
2022-11-06 11:45:35 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		//Scanner sc = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] res = new int[N][M];
-        StringBuffer sb = new StringBuffer();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+        BigInteger N = new BigInteger(str);
+        str = sc.next();
+        BigInteger M = new BigInteger(str);
         
-        for(int i = 0; i < N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j = 0; j < M; j++) {
-        		res[i][j] = Integer.parseInt(st.nextToken());
-        	}
-        }
-        for(int i = 0; i < N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j = 0; j < M; j++) {
-        		res[i][j] += Integer.parseInt(st.nextToken());
-        		sb.append(res[i][j] + " ");
-        	}
-        	sb.append("\n");
-        }
-        
-		
-		System.out.println(sb);
+        System.out.println(N.divide(M));
+        System.out.println(N.remainder(M));
     }
 }
 
