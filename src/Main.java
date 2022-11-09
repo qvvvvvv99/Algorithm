05daa1adcb51f6bf97212ea8dcfdf775b404b4b5
KimJH <qvvvvvv99@naver.com>
2022-11-09 20:24:56 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		System.out.println(".  .   .");
-		System.out.println("|  | _ | _. _ ._ _  _");
-		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
     }
 }
 
