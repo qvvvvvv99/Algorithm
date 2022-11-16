777bfa8e94239a7ab4a89493a342df84612d2c3e
KimJH <qvvvvvv99@naver.com>
2022-11-16 16:40:08 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Scanner sc = new Scanner(System.in);
 //		int N = sc.nextInt();
-		
-		System.out.println("I'm Sexy");
-    }
+
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+		System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+	}
 }
 
 //public static boolean isPrime(int num) {
