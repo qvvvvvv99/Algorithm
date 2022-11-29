66aad50372ec1faae7d23b2c3b211e4654b106c9
KimJH <qvvvvvv99@naver.com>
2022-11-29 23:22:17 +0900

    변경

--- src/AlgoStudy/Week19/BJ1083.java
+++ src/AlgoStudy/Week19/BJ1083.java
@@ -22,24 +22,29 @@ public class BJ1083 {
 		ArrayList<Integer> sort = new ArrayList<>(list);
 		Collections.sort(sort, Collections.reverseOrder());
 
+		// cnt : 확정된 자리 수
 		int cnt = 0;
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N-1; i++) {
+			// i번째 가질 수 있는 최대 수
 			int max = sort.get(i);
+			// 배열에서 max의 인덱스번호
 			int idx = list.indexOf(max);
+			// idx - cnt : 해당 수가 이동해야할 거리
 			if (S >= idx - cnt) {
+				// 이동
 				list.remove(idx);
 				list.add(cnt, max);
 				S -= (idx - cnt);
 				if(S == 0)
 					break;
-				cnt++;
+				cnt++;	// 자리수 갱신
 			}
 		}
 
 		StringBuffer sb = new StringBuffer();
 		for (int i: list)
 			sb.append(i + " ");
-		System.out.println(sb.toString());
+		System.out.print(sb.toString());
 	}
 
 }
