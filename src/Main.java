9434229dbefb1b6d58f09c3733c66df6c3feaeb1
KimJH <qvvvvvv99@naver.com>
2022-12-11 16:55:47 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        long temp = 0;
-        for (int i = 0; i < input.length(); i++) {
-            temp = (temp * 10 + (input.charAt(i) - '0')) % 20000303;
+        Scanner sc = new Scanner(System.in);
+        int res = 0;
+        for (int i = 0; i < 5; i++) {
+        	res += sc.nextInt();
         }
         
-        System.out.println(temp);
+        System.out.println(res);
     }
 }
 
