51b34cea1f35de55990c1273a986570cbd5742dd
KimJH <qvvvvvv99@naver.com>
2022-12-16 23:59:11 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int res = 0;
-        for (int i = 0; i < 5; i++) {
-        	res += sc.nextInt();
-        }
+        long a = sc.nextLong();
+        long b = sc.nextLong();
         
-        System.out.println(res);
+        System.out.println(Math.abs(a-b));
     }
 }
 
