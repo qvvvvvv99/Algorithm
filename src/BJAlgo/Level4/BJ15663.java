3f8352108176b857280e257278f84367ce9b16e8
KimJH <qvvvvvv99@naver.com>
2022-12-17 00:25:31 +0900

    백준 level4 문제풀이

--- src/BJAlgo/Level4/BJ15663.java
+++ src/BJAlgo/Level4/BJ15663.java
@@ -1,10 +1,57 @@
 package BJAlgo.Level4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
 public class BJ15663 {
+	static int N, M;
+	static int[] ary;
+	static int[] res;
+	static boolean[] visited;
+	static LinkedHashSet<String> hs = new LinkedHashSet<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		ary = new int[N];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+			ary[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(ary);
+		
+		res = new int[M];
+		dfs(0);
+		
+		hs.forEach(System.out::println);
+	}
+	
+	static void dfs(int idx) {
+		if(idx == M) {
+			StringBuffer sb = new StringBuffer();
+			for(int num : res)
+				sb.append(num).append(' ');
+			hs.add(sb.toString());
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(visited[i])
+				continue;
+			visited[i] = true;
+			res[idx] = ary[i];
+			dfs(idx+1);
+			visited[i] = false;
+		}
 	}
 
 }
