7380ddf35930e72b086152727552904a2008274d
KimJH <qvvvvvv99@naver.com>
2022-12-26 14:58:06 +0900

    풀이 수정

--- src/AlgoStudy/Week22/BJ1069.java
+++ src/AlgoStudy/Week22/BJ1069.java
@@ -23,20 +23,21 @@ public class BJ1069 {
 		double diagoLen = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2));
 		int div = 0;
 
+		// 걸어가기
 		res = diagoLen;
 		div = (int) (diagoLen / D);
-		// 한번의 점프가 목적지까지 거리보다 짧을 때
+		// 한번의 점프가 목적지까지 거리보다 길 때
 		if (div == 0) {
 			// 점프 한번하고 넘은 거리 돌아오기
 			res = Math.min(res, T + D - diagoLen);
-			// 두번 점프해서 도착하기
+			// 두번 꺽어 점프해서 도착하기
 			res = Math.min(res, 2 * T);
 		} 
-		// 한번의 점프가 목적지까지 거리보다 길 때
+		// 한번의 점프가 목적지까지 거리보다 짧을 때
 		else {
-			// 직전까지 점프하고 더 가기
+			// 직전까지 점프하고 걸어서 도착하기
 			res = Math.min(res, T * div + diagoLen - D * div);
-			// 넘어가는 만큼 점프하고 돌아가기
+			// 전전까지 점프하고 두번 꺽어 점프해서 도착하기
 			if(T * (div + 1) < res)
 				res = T * (div + 1);
 		}
