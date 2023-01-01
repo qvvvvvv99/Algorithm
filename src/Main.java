c0b2b062b7c72613fbbb561eb2e1ff470d045b3e
KimJH <qvvvvvv99@naver.com>
2023-01-01 19:46:13 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
+        int Ur = sc.nextInt();
+        int Tr = sc.nextInt();
+        int Uo = sc.nextInt();
+        int To = sc.nextInt();
         
-        System.out.println("A");
+        System.out.println(56*Ur+24*Tr+14*Uo+6*To);
     }
 }
 
