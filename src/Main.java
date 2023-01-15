443cf26a98f1dcb7ad46202c0e2c2ecb0b8e5bfc
KimJH <qvvvvvv99@naver.com>
2023-01-15 23:06:30 +0900

    백준 문제

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int sum = 0;
         
-        for(int i = 0; i < n; i++) {
-        	int p = sc.nextInt();
-        	int t = sc.nextInt();
-        	System.out.println(p - (t/7) + (t/4));
+        for(int i = 0; i < 4; i++) {
+        	int s = sc.nextInt();
+        	sum += s;
         }
+    	System.out.println(sum / 60);
+    	System.out.println(sum % 60);
     }
 }
 
