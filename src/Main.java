373d251295415cd753de032bca742a33a4537b16
KimJH <qvvvvvv99@naver.com>
2023-02-04 11:30:22 +0900

    현대오토에버 1번

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,56 @@ public class Main {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        StringBuffer sb = new StringBuffer();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] map = new int[N][N];
         
-        for(int i = 0; i < N; i++) {
-        	String str = sc.next();
-            sb.append(str.toLowerCase()).append("\n");
+        int num = 2;
+        int len = 1;
+        int x = 0;
+        int y = 0;
+        int dir = 1;
+        map[0][0] = 1;
+        
+        for(int i = 1; i < N; i++) {
+        	if(i % 2 == 0) {
+        		x+=1;
+        		map[x][y] = num;
+        		num++;
+        		dir = 1;
+        		for(int j = 0; j < len; j++) {
+        			y += dir;
+        			map[x][y] = num;
+        			num++;
+        		}
+        		dir = -1;
+        		for(int j = 0; j < len; j++) {
+        			x += dir;
+        			map[x][y] = num;
+        			num++;
+        		}
+        	}
+        	else {
+        		y+=1;
+        		map[x][y] = num;
+        		num++;
+        		dir = 1;
+        		for(int j = 0; j < len; j++) {
+        			x += dir;
+        			map[x][y] = num;
+        			num++;
+        		}
+        		dir = -1;
+        		for(int j = 0; j < len; j++) {
+        			y += dir;
+        			map[x][y] = num;
+        			num++;
+        		}
+        	}
+        	len++;
         }
         
-        System.out.print(sb);
+        System.out.println(map[r-1][c-1]);
     }
 }
 
