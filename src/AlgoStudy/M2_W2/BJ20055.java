4234fb11764d4d6009a8e4539f75aafc9cd11e02
KimJH <qvvvvvv99@naver.com>
2023-02-21 14:13:46 +0900

    백준 20055 수정 풀이

--- src/AlgoStudy/M2_W2/BJ20055.java
+++ src/AlgoStudy/M2_W2/BJ20055.java
@@ -1,93 +1,79 @@
 package AlgoStudy.M2_W2;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ20055 {
-	static int N, K;
-	static int[] belt;
-	static int[] robot;
-	static boolean[] exist;
-	static int first;
-	static int head = 0, tail = -1;
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		belt = new int[2*N];
-		robot = new int[2*N];
-		exist = new boolean[2*N];
-		Arrays.fill(robot, -1);
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < 2*N; i++)
-			belt[i] = Integer.parseInt(st.nextToken());
-		
-		int cnt = 0;
-		while(true) {
-			cnt++;
-			// rotate belt
-			first--;
-			if(first < 0)
-				first = 2*N-1;
-			
-			dropRobot();
-			
-			// move robot
-			moveRobot();
-			
-			dropRobot();
-			
-			// ¿Ã¸®±â
-			if(belt[first] > 0) {
-				tail++;
-				tail %= 2 * N;
-				robot[tail] = first;
-				belt[first]--;
-				exist[first] = true;
-				if(belt[first] == 0)
-					K--;
-			}
-			
-			if(K == 0)
-				break;
-		}
-		
-		System.out.println(cnt);
-	}
-	
-	static void moveRobot() {
-		int num = head;
-		int idx, next;
-		while(robot[num] >= 0) {
-			idx = robot[num];
-			next = (idx + 1) % (2*N);
-			if(!exist[next] && belt[next] > 0) {
-				robot[num] = next;
-				belt[next]--;
-				exist[next] = true;
-				exist[idx] = false;
-				if(belt[next] == 0)
-					K--;
-			}
-			num++;
-			num %= 2 * N;
-		}
-	}
-	
-	static void dropRobot() {
-		if(exist[(first + N -1) % (2*N)]) {
-			exist[(first + N -1) % (2*N)] = false;
-			robot[head] = -1;
-			head++;
-			head %= 2 * N;
-		}
-	}
-
+    
+    static int n, k, map[], left, right;
+    static boolean robot[];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stz.nextToken());
+        k = Integer.parseInt(stz.nextToken());
+        map = new int[2*n];
+        robot = new boolean[n];
+        stz = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 2* n; i++)
+            map[i] = Integer.parseInt(stz.nextToken());
+        left = 0;
+        right = n;
+        
+        int count = 0;
+        while(k > 0) {
+            count++;
+            moveBelt();
+            moveRobot();
+            newRobot();
+        }
+        
+        System.out.println(count);
+    }
+    
+    public static void moveBelt() {
+        left--;
+        right--;
+        if(left == -1)
+            left = 2*n-1;
+        if(right == -1)
+            right = 2*n-1;
+        
+        for(int i = n-2; i >= 0; i--) {
+            if(robot[i]) {
+                robot[i] = false;
+                if(i+1 < n-1)
+                    robot[i+1] = true;
+            }
+        }
+    }
+    
+    public static void moveRobot() {
+        for(int i = n-2; i >= 0; i--) {
+            if(robot[i]) {
+                int next = left+i+1;
+                if(next >= 2*n)
+                    next -= 2*n;
+                if(!robot[i+1] && map[next] >= 1) {
+                    robot[i] = false;
+                    if(i+1 < n-1)
+                        robot[i + 1] = true;
+                    map[next]--;
+                    if(map[next] == 0)
+                        k--;
+                }
+            }
+        }
+    }
+    
+    public static void newRobot() {
+        if(!robot[0] && map[left] > 0) {
+            robot[0] = true;
+            map[left]--;
+            if(map[left] == 0)
+                k--;
+        }
+    }
+    
 }
